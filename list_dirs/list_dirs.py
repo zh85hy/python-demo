3440c4b560054f33dece4765b49cc89f0a151082
zh85hy <zh85hy@github.com>
2017-12-29 00:18:05 +0800

    Update py file.

--- list_dirs/list_dirs.py
+++ list_dirs/list_dirs.py
@@ -5,7 +5,19 @@ listing_path = '/Users/hy/Desktop/Learning_Python/python-demo/list_dirs/testing_
 write_file_name = 'dirs_tree.txt'
 
 
-def list_dirs_then_write_into_file(dir_name, write_file, level=1):
+def list_dirs_then_print(dir_name, level=1):
+    if level == 1:
+        print(dir_name)
+    lists = os.listdir(dir_name)
+    # print(lists)
+    for path in lists:
+        next_path = os.path.join(dir_name, path)
+        print(next_path)
+        if os.path.isdir(next_path):
+            list_dirs_then_print(next_path, level+1)
+
+
+def list_dirs_tree_then_write(dir_name, write_file, level=1):
     if level == 1:
         print(dir_name)
         with open(write_file, 'a') as f:
@@ -13,15 +25,16 @@ def list_dirs_then_write_into_file(dir_name, write_file, level=1):
     lists = os.listdir(dir_name)
     # print(lists)
     for path in lists:
-        # print('|    '*(level-1)+'|----'+path)
+        print('|    '*(level-1)+'|----'+path)
         with open(write_file, 'a') as f:
             f.write('|    '*(level-1)+'|----'+path+'\n')
-        sub_path = os.path.join(dir_name, path)
-        if os.path.isdir(sub_path):
-            list_dirs_then_write_into_file(sub_path, write_file, level+1)
+        next_path = os.path.join(dir_name, path)
+        if os.path.isdir(next_path):
+            list_dirs_tree_then_write(next_path, write_file, level+1)
 
 
 if __name__ == '__main__':
+    # list_dirs_then_print(listing_path)
     if os.path.exists(write_file_name):
         os.remove(write_file_name)
-    list_dirs_then_write_into_file(listing_path, write_file_name)
+    list_dirs_tree_then_write(listing_path, write_file_name)
